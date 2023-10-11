4b4013c1cf09209944f90d74b85cf16db5d45cc5
GabFor07 <gabrielafortoulct@gmail.com>
2023-10-11 11:10:01 -0700

    Ghost chase pacman and run away from pacman

--- Movement/Ghost.cs
+++ Movement/Ghost.cs
@@ -4,29 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks.Dataflow;
 using System.Timers;
+using static Movement.Ghost;
 
 namespace Movement
 {
-    internal class Ghost:Character
+
+    internal class Ghost : Character
     {
+        public enum Direction
+        {
+            Up,
+            Right,
+            Down,
+            Left,
+            Stop
+        }
         //List<Direction> pathList = new List<Direction>();
         int pathListIndex = 0;
         bool weakness;
+        int[][] pathCoorArr;
+        int i = 0;
+        List<Direction> pathList;
+        List<Direction> chasePath;
 
-        public Ghost(int x,int y, int[][] coorArr)
+        public Ghost(int x, int y, int[][] coorArr)
         {
             X = x;
             Y = y;
             PathCoorArr = coorArr;
             PathList = new List<Direction>();
+            chasePath = new List<Direction>();
             Type = "ghost";
             Symbol = '†';
             weakness = false;
         }
 
+        public int[][] PathCoorArr
+        {
+            get { return pathCoorArr; }
+            set { pathCoorArr = value; }
+        }
+
+        public List<Direction> PathList
+        {
+            get { return pathList; }
+            set { pathList = value; }
+        }
+
         public void SetRoad(Map levelMap)
         {
-            int[] currentPos = { X,Y };
+            int[] currentPos = { X, Y };
             Direction direction = Direction.Stop;
 
             //Console.WriteLine("StartPoint {0} EndPoint {1}", currentPos[0], coorArr[0][0]);
@@ -66,9 +93,82 @@ namespace Movement
 
         }
 
-        public void Movement(Map levelMap)
+        public void Movement(Map levelMap, List<Direction> pathToMove)
         {
-            if(pathListIndex == PathList.Count)
+            if (Y == PathCoorArr[1][1] && X == PathCoorArr[1][0])
+            {
+                ReversePath();
+
+            }
+            if (i < pathToMove.Count)
+            {
+                if (pathToMove[i] == Direction.Left)
+                {
+                    if (levelMap.map[Y, X - 1] != '#' && levelMap.map[Y, X - 1] == '-')
+                    {
+                        levelMap.map[Y, X] = '-';
+                        X--;
+                    }
+                    else
+                    {
+                        levelMap.map[Y, X] = ' ';
+                        X--;
+                    }
+                    Spawn(levelMap);
+                }
+                else if (pathToMove[i] == Direction.Right)
+                {
+                    if (levelMap.map[Y, X + 1] != '#' && levelMap.map[Y, X + 1] == '-')
+                    {
+                        levelMap.map[Y, X] = '-';
+                        X++;
+                    }
+                    else
+                    {
+                        levelMap.map[Y, X] = ' ';
+                        X++;
+                    }
+                    Spawn(levelMap);
+                }
+                else if (pathToMove[i] == Direction.Up)
+                {
+                    if (levelMap.map[Y - 1, X] != '#' && levelMap.map[Y - 1, X] == '-')
+                    {
+                        levelMap.map[Y, X] = '-';
+                        Y--;
+                    }
+                    else
+                    {
+                        levelMap.map[Y, X] = ' ';
+                        Y--;
+                    }
+                    Spawn(levelMap);
+                }
+                else if (pathToMove[i] == Direction.Down)
+                {
+                    if (levelMap.map[Y + 1, X] != '#' && levelMap.map[Y + 1, X] == '-')
+                    {
+                        levelMap.map[Y, X] = '-';
+                        Y++;
+                    }
+                    else
+                    {
+                        levelMap.map[Y, X] = ' ';
+                        Y++;
+                    }
+                    Spawn(levelMap);
+                }
+                else
+                {
+                    Console.WriteLine("Err");
+                }
+                i++;
+            }
+        }
+
+        public void ReversePath()
+        {
+            if (pathListIndex == PathList.Count)
             {
                 PathList.Reverse();
                 List<Direction> pathListReverse = new List<Direction>();
@@ -96,72 +196,6 @@ namespace Movement
                 PathList = pathListReverse;
                 pathListIndex = 0;
             }
-            if (PathList[pathListIndex] == Direction.Left)
-            {
-                if (levelMap.map[Y, X - 1] != '#' && levelMap.map[Y, X - 1] == '-')
-                {
-                    levelMap.map[Y, X] = '-';
-                    X--;
-                }
-                else
-                {
-                    levelMap.map[Y, X] = ' ';
-                    X--;
-                }
-                Spawn(levelMap);
-            }
-            else if (PathList[pathListIndex] == Direction.Right)
-            {
-                if (levelMap.map[Y, X + 1] != '#' && levelMap.map[Y, X + 1] == '-')
-                {
-                    levelMap.map[Y, X] = '-';
-                    X++;
-                }
-                else
-                {
-                    levelMap.map[Y, X] = ' ';
-                    X--;
-                }
-                Spawn(levelMap);
-            }
-            else if (PathList[pathListIndex] == Direction.Up)
-            {
-                if (levelMap.map[Y - 1, X] != '#' && levelMap.map[Y - 1, X] == '-')
-                {
-                    levelMap.map[Y, X] = '-';
-                    Y--;
-                }
-                else
-                {
-                    levelMap.map[Y, X] = ' ';
-                    X--;
-                }
-                Spawn(levelMap);
-            }
-            else if (PathList[pathListIndex] == Direction.Down)
-            {
-                if (levelMap.map[Y + 1, X] != '#' && levelMap.map[Y + 1, X] == '-')
-                {
-                    levelMap.map[Y, X] = '-';
-                    Y++;
-                }
-                else
-                {
-                    levelMap.map[Y, X] = ' ';
-                    X--;
-                }
-                Spawn(levelMap);
-            }
-            else
-            {
-                Console.WriteLine("Err");
-            }
-
-            if(pathListIndex != PathList.Count)
-            {
-                pathListIndex++;
-            }
-            
         }
 
         public bool Weakness
@@ -170,5 +204,287 @@ namespace Movement
             set { weakness = value; }
         }
 
+        public void Chase(PacMan pacMan, Map levelMap)
+        {
+            if (pacMan.X >= X && pacMan.Y > Y)
+            {
+                if (!TryDown(levelMap))
+                {
+                    if (!TryRight(levelMap))
+                    {
+                        if (!TryUp(levelMap))
+                        {
+                            if (!TryLeft(levelMap))
+                            {
+                                Console.WriteLine("Error ghost trapped");
+                            }
+                        }
+                    }
+                }
+            }
+            else if (pacMan.X < X && pacMan.Y > Y)
+            {
+                if (!TryDown(levelMap))
+                {
+                    if (!TryLeft(levelMap))
+                    {
+                        if (!TryUp(levelMap))
+                        {
+                            if (!TryRight(levelMap))
+                            {
+                                Console.WriteLine("Error ghost trapped");
+                            }
+                        }
+                    }
+                }
+            }
+            else if (pacMan.X >= X && pacMan.Y <= Y)
+            {
+                if (!TryUp(levelMap))
+                {
+                    if (!TryRight(levelMap))
+                    {
+                        if (!TryDown(levelMap))
+                        {
+                            if (!TryLeft(levelMap))
+                            {
+                                Console.WriteLine("Error ghost trapped");
+                            }
+                        }
+                    }
+                }
+            }
+            else if (pacMan.X < X && pacMan.Y <= Y)
+            {
+                if (!TryUp(levelMap))
+                {
+                    if (!TryLeft(levelMap))
+                    {
+                        if (!TryDown(levelMap))
+                        {
+                            if (!TryRight(levelMap))
+                            {
+                                Console.WriteLine("Error ghost trapped");
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.Write("We have a problem");
+            }
+
+
+
+        }
+
+        public void RunAway(PacMan pacMan, Map levelMap)
+        {
+            if (pacMan.X >= X && pacMan.Y > Y)
+            {
+                if (!TryUp(levelMap))
+                {
+                    if (!TryLeft(levelMap))
+                    {
+                        if (!TryDown(levelMap))
+                        {
+                            if (!TryRight(levelMap))
+                            {
+                                Console.WriteLine("Error ghost trapped");
+                            }
+                        }
+                    }
+                }
+            }
+            else if (pacMan.X < X && pacMan.Y > Y)
+            {
+                if (!TryUp(levelMap))
+                {
+                    if (!TryRight(levelMap))
+                    {
+                        if (!TryDown(levelMap))
+                        {
+                            if (!TryLeft(levelMap))
+                            {
+                                Console.WriteLine("Error ghost trapped");
+                            }
+                        }
+                    }
+                }
+            }
+            else if (pacMan.X >= X && pacMan.Y <= Y)
+            {
+                if (!TryDown(levelMap))
+                {
+                    if (!TryLeft(levelMap))
+                    {
+                        if (!TryUp(levelMap))
+                        {
+                            if (!TryRight(levelMap))
+                            {
+                                Console.WriteLine("Error ghost trapped");
+                            }
+                        }
+                    }
+                }
+            }
+            else if (pacMan.X < X && pacMan.Y <= Y)
+            {
+                if (!TryDown(levelMap))
+                {
+                    if (!TryRight(levelMap))
+                    {
+                        if (!TryUp(levelMap))
+                        {
+                            if (!TryLeft(levelMap))
+                            {
+                                Console.WriteLine("Error ghost trapped");
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.Write("We have a problem");
+            }
+
+
+
+        }
+
+        bool TryDown(Map levelMap)
+        {
+            if (levelMap.map[Y + 1, X] != '#' && (chasePath.Count == 0 || chasePath.Last() != Direction.Down))
+            {
+                if (levelMap.map[Y + 1, X] == '-')
+                {
+                    levelMap.map[Y, X] = '-';
+                    chasePath.Add(Direction.Up);
+                    Y++;
+                    Spawn(levelMap);
+                    return true;
+                }
+                else
+                {
+                    chasePath.Add(Direction.Up);
+                    Y++;
+                    Spawn(levelMap);
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        bool TryUp(Map levelMap)
+        {
+            if (levelMap.map[Y - 1, X] != '#' && (chasePath.Count == 0 || chasePath.Last() != Direction.Up))
+            {
+                if (levelMap.map[Y - 1, X] == '-')
+                {
+                    levelMap.map[Y, X] = '-';
+                    chasePath.Add(Direction.Down);
+                    Y--;
+                    Spawn(levelMap);
+                    return true;
+                }
+                else
+                {
+                    chasePath.Add(Direction.Down);
+                    Y--;
+                    Spawn(levelMap);
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        bool TryRight(Map levelMap)
+        {
+            if (levelMap.map[Y, X + 1] != '#' && (chasePath.Count == 0 || chasePath.Last() != Direction.Right))
+            {
+                if (levelMap.map[Y, X + 1] == '-')
+                {
+                    levelMap.map[Y, X] = '-';
+                    chasePath.Add(Direction.Left);
+                    X++;
+                    Spawn(levelMap);
+                    return true;
+                }
+                else
+                {
+                    chasePath.Add(Direction.Left);
+                    X++;
+                    Spawn(levelMap);
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        bool TryLeft(Map levelMap)
+        {
+            if (levelMap.map[Y, X - 1] != '#' && (chasePath.Count == 0 || chasePath.Last() != Direction.Left))
+            {
+                if (levelMap.map[Y, X - 1] == '-')
+                {
+                    levelMap.map[Y, X] = '-';
+                    chasePath.Add(Direction.Right);
+                    X--;
+                    Spawn(levelMap);
+                    return true;
+                }
+                else
+                {
+                    chasePath.Add(Direction.Right);
+                    X--;
+                    Spawn(levelMap);
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public void ReverseChase()
+        {
+            chasePath.Reverse();
+            List<Direction> chasePathReverse = new List<Direction>();
+            foreach (var item in chasePath)
+            {
+                if (item == Direction.Left)
+                {
+                    chasePathReverse.Add(Direction.Right);
+                }
+                else if (item == Direction.Up)
+                {
+                    chasePathReverse.Add(Direction.Down);
+                }
+                else if (item == Direction.Down)
+                {
+                    chasePathReverse.Add(Direction.Up);
+                }
+                else if (item == Direction.Right)
+                {
+                    chasePathReverse.Add(Direction.Left);
+                }
+            }
+        }
     }
 }
